4cab6c8661c3807782991907597454dd427c9f0c
Julien AZOUZ <juazouz@e1r8p6.42.fr>
2019-01-03 13:30:17 +0100

    libft update

--- libft/srcs/math/ft_abs.c
+++ libft/srcs/math/ft_abs.c
@@ -1,21 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*   ft_abs.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juazouz <juazouz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/12 19:44:22 by juazouz           #+#    #+#             */
-/*   Updated: 2018/12/28 19:19:04 by juazouz          ###   ########.fr       */
+/*   Created: 2018/12/28 18:48:35 by juazouz           #+#    #+#             */
+/*   Updated: 2018/12/28 18:48:58 by juazouz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-void			ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
+int	ft_abs(int val)
 {
-	if (del != NULL)
-		del((*alst)->content, (*alst)->content_size);
-	free(*alst);
-	*alst = NULL;
+	if (val < 0)
+		return (-val);
+	return (val);
 }
